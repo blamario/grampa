98cb8de61e09fc0600d5caad72e55c3550548f85
Mario Blažević <blamario@yahoo.com>
2019-10-20 14:44:40 -0400

    Running the README through cabal-doctest

--- grammatical-parsers/test/Doctest.hs
+++ grammatical-parsers/test/Doctest.hs
@@ -2,4 +2,6 @@ import Build_doctests (flags, pkgs, module_sources)
 import Test.DocTest (doctest)
 
 main :: IO ()
-main = doctest (flags ++ pkgs ++ module_sources)
+main = do
+    doctest (flags ++ pkgs ++ module_sources)
+    doctest (flags ++ pkgs ++ ["-pgmL", "markdown-unlit", "-isrc", "test/README.lhs"])
