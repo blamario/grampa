3208771e42f3f7ae8d3c244212310a0618a23cbb
Mario Blažević <blamario@yahoo.com>
2019-10-28 22:36:36 -0400

    Dropped the newtype wrappers from attribution and synthesis

--- deep-transformations/src/Transformation/AG.hs
+++ deep-transformations/src/Transformation/AG.hs
@@ -29,18 +29,18 @@ knit r chSem = Rank2.Arrow knit'
 class Attribution t g f where
    attribution :: t -> f (g (Semantics t) (Semantics t)) -> Rule t g
    bequest :: forall sem . sem ~ Semantics t =>
-              t -> f (g sem sem) -> Inherited t (g sem sem) -> g sem (Synthesized t)
+              t -> f (g sem sem) -> Atts (Inherited t) (g sem sem) -> g sem (Synthesized t)
               -> g sem (Inherited t)
    synthesis   :: forall sem . sem ~ Semantics t =>
-                  t -> f (g sem sem) -> Inherited t (g sem sem) -> g sem (Synthesized t)
-               -> Synthesized t (g sem sem)
-   attribution t l (i, s) = (synthesis t l i s, bequest t l i s)
-   bequest t l i s = snd (attribution t l (i, s))
-   synthesis t l i s = fst (attribution t l (i, s))
+                  t -> f (g sem sem) -> Atts (Inherited t) (g sem sem) -> g sem (Synthesized t)
+               -> Atts (Synthesized t) (g sem sem)
+   attribution t l (Inherited i, s) = (Synthesized $ synthesis t l i s, bequest t l i s)
+   bequest t l i s = snd (attribution t l (Inherited i, s))
+   synthesis t l i s = syn (fst $ attribution t l (Inherited i, s))
    {-# Minimal attribution | bequest, synthesis #-}
 
 class Inheritable t g where
-   passOnInheritance :: sem ~ Semantics t => Inherited t (g sem sem) -> g sem sem -> g sem (Inherited t)
+   passOnInheritance :: sem ~ Semantics t => Atts (Inherited t) (g sem sem) -> g sem sem -> g sem (Inherited t)
 
 -- | Drop-in implementation of 'Transformation.<$>'
 mapDefault :: (q ~ Semantics t, x ~ g q q, Rank2.Apply (g q), Attribution t g p)
