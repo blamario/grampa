cca0612e8352c4f91ec43188fc9d924b847d0f7d
Mario Blažević <blamario@yahoo.com>
2019-11-23 15:07:18 -0500

    Split the last parameter of the Attribution class into deep and shallow

--- deep-transformations/src/Transformation/AG.hs
+++ deep-transformations/src/Transformation/AG.hs
@@ -7,6 +7,8 @@ import Data.Functor.Identity
 import qualified Rank2
 import Transformation (Transformation, Domain, Codomain)
 import qualified Transformation
+import qualified Transformation.Deep as Deep
+import qualified Transformation.Full as Full
 import qualified Transformation.Shallow as Shallow
 
 type family Atts (f :: * -> *) a
@@ -29,13 +31,13 @@ knit r chSem = Rank2.Arrow knit'
             where (syn, chInh) = r (inh, chSyn)
                   chSyn = chSem Rank2.<*> chInh
 
-class Attribution t g f where
-   attribution :: t -> f (g (Semantics t) (Semantics t)) -> Rule t g
-   bequest :: forall sem . sem ~ Semantics t =>
-              t -> f (g sem sem) -> Atts (Inherited t) (g sem sem) -> g sem (Synthesized t)
-              -> g sem (Inherited t)
+class Attribution t g deep shallow where
+   attribution :: t -> shallow (g deep deep) -> Rule t g
+   bequest     :: forall sem . sem ~ Semantics t =>
+                  t -> shallow (g deep deep) -> Atts (Inherited t) (g sem sem) -> g sem (Synthesized t)
+               -> g sem (Inherited t)
    synthesis   :: forall sem . sem ~ Semantics t =>
-                  t -> f (g sem sem) -> Atts (Inherited t) (g sem sem) -> g sem (Synthesized t)
+                  t -> shallow (g deep deep) -> Atts (Inherited t) (g sem sem) -> g sem (Synthesized t)
                -> Atts (Synthesized t) (g sem sem)
    attribution t l (Inherited i, s) = (Synthesized $ synthesis t l i s, bequest t l i s)
    bequest t l i s = snd (attribution t l (Inherited i, s))
@@ -54,7 +56,14 @@ passDown :: (sem ~ Semantics t, Shallow.Functor (Inherited t (g sem sem)) (g sem
 passDown inheritance node = inheritance Shallow.<$> node
 
 -- | Drop-in implementation of 'Transformation.$'
-applyDefault :: (q ~ Semantics t, x ~ g q q, Rank2.Apply (g q), Attribution t g p)
-           => (p x -> x) -> t -> p x -> q x
-applyDefault getSemantics t x = knit (attribution t x) (getSemantics x)
+applyDefault :: (q ~ Semantics t, x ~ g q q, Rank2.Apply (g q), Attribution t g q p)
+             => (forall a. p a -> a) -> t -> p x -> q x
+applyDefault extract t x = knit (attribution t x) (extract x)
 {-# INLINE applyDefault #-}
+
+-- | Drop-in implementation of 'Full.<$>'
+fullMapDefault :: (p ~ Domain t, q ~ Semantics t, q ~ Codomain t, x ~ g q q, Rank2.Apply (g q),
+                   Deep.Functor t g, Attribution t g p p)
+               => (forall a. p a -> a) -> t -> p (g p p) -> q (g q q)
+fullMapDefault extract t local = knit (attribution t local) (t Deep.<$> extract local)
+{-# INLINE fullMapDefault #-}
