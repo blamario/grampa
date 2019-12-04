74c7b5c244a3dd1dad9074fa53b4442e0ff304df
Mario Blažević <blamario@yahoo.com>
2019-12-03 22:50:45 -0500

    Added the deep-transformations README doctest to Cabal

---
+++ deep-transformations/test/Doctest.hs
@@ -0,0 +1,7 @@
+import Build_doctests (flags, pkgs, module_sources)
+import Test.DocTest (doctest)
+
+main :: IO ()
+main = do
+    doctest (flags ++ pkgs ++ module_sources)
+    doctest (flags ++ pkgs ++ ["-pgmL", "markdown-unlit", "-isrc", "test/README.lhs"])
