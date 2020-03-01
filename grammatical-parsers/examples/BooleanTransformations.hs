251bc48855277d53e379dbdfb7550b01a4e17971
Mario Blažević <blamario@protonmail.com>
2020-03-01 15:12:33 -0500

    Example of storing comments alongside whitespace

--- grammatical-parsers/examples/BooleanTransformations.hs
+++ grammatical-parsers/examples/BooleanTransformations.hs
@@ -3,21 +3,23 @@
 module Main where
 
 import Control.Applicative ((<|>), empty)
-import Control.Monad (guard)
+import Control.Arrow (first)
+import Control.Monad (guard, join)
 import Data.Char (isSpace)
 import Data.Functor.Compose (Compose(..))
 import Data.Functor.Identity (Identity(..))
 import System.Environment (getArgs)
 import Text.Parser.Token (TokenParsing(..), symbol)
 import qualified Text.Grampa
-import Text.Grampa (TokenParsing(someSpace), LexicalParsing(lexicalWhiteSpace, someLexicalSpace), GrammarBuilder, ParseResults,
+import Text.Grampa (TokenParsing(someSpace), LexicalParsing(lexicalComment, lexicalWhiteSpace, someLexicalSpace),
+                    GrammarBuilder, ParseResults,
                     fixGrammar, parseComplete,
-                    identifier, keyword, takeCharsWhile)
+                    char, identifier, keyword, takeCharsWhile)
 import Text.Grampa.ContextFree.SortedMemoizing.Transformer (ParserT, lift, tmap)
 import qualified Boolean
 import Boolean(Boolean(..))
 
-type Parser = ParserT ((,) [String])
+type Parser = ParserT NodeWrap
 
 data AST f = And (f (AST f)) (f (AST f))
            | Or (f (AST f)) (f (AST f))
@@ -37,29 +39,37 @@ instance Boolean.BooleanDomain (NodeWrap (AST NodeWrap)) where
 binary :: Applicative f => (f (AST f) -> f (AST f) -> AST f) -> f (AST f) -> f (AST f) -> f (AST f)
 binary f a b = pure (f a b)
 
-type NodeWrap = (,) [String]
+type NodeWrap = (,) [[Either WhiteSpace Comment]]
+newtype Comment    = Comment String    deriving Show
+newtype WhiteSpace = WhiteSpace String deriving Show
+
 type Grammar = Boolean.Boolean (NodeWrap (AST NodeWrap))
 
 main :: IO ()
 main = do args <- concat <$> getArgs
-          let tree = (getCompose . getCompose . Boolean.expr $
-                      parseComplete (fixGrammar grammar) args :: ParseResults String [([String], NodeWrap (AST NodeWrap))])
+          let tree = (getCompose . fmap join . getCompose . Boolean.expr $
+                      parseComplete (fixGrammar grammar) args :: ParseResults String [NodeWrap (AST NodeWrap)])
           print tree
           case tree
-             of Right results -> mapM_ (putStrLn . reconstructed 0 . snd) results
+             of Right results -> mapM_ (print . reconstructed 0) results
                 _ -> pure ()
 
 reconstructed :: Int -> NodeWrap (AST NodeWrap) -> String
 reconstructed prec (ws, node) = serialized prec ws node
 
-serialized :: Int -> [String] -> AST NodeWrap -> String
-serialized prec [follow] (Or left right) | prec < 1 = reconstructed 1 left <> "||" <> follow <> reconstructed 2 right
-serialized prec [follow] (And left right) | prec < 2 = reconstructed 2 left <> "&&" <> follow <> reconstructed 1 right
-serialized prec [follow] (Not expr) | prec < 3 = "not" <> follow <> reconstructed 2 expr
-serialized _ [follow] (Literal True) = "True" <> follow
-serialized _ [follow] (Literal False) = "False" <> follow
-serialized _ [follow] (Variable name) = name <> follow
-serialized _ (precede:rest) node = "(" <> precede <> serialized 0 (init rest) node <> ")" <> last rest
+serialized :: Int -> [[Either WhiteSpace Comment]] -> AST NodeWrap -> String
+serialized prec [follow] (Or left right) | prec < 1 = reconstructed 1 left <> "||" <> whiteString follow <> reconstructed 0 right
+serialized prec [follow] (And left right) | prec < 2 = reconstructed 2 left <> "&&" <> whiteString follow <> reconstructed 1 right
+serialized prec [follow] (Not expr) | prec < 3 = "not" <> whiteString follow <> reconstructed 2 expr
+serialized _ [follow] (Literal True) = "True" <> whiteString follow
+serialized _ [follow] (Literal False) = "False" <> whiteString follow
+serialized _ [follow] (Variable name) = name <> whiteString follow
+serialized _ (precede:rest) node = "(" <> whiteString precede <> serialized 0 (init rest) node <> ")" <> whiteString (last rest)
+
+whiteString :: [Either WhiteSpace Comment] -> String
+whiteString (Left (WhiteSpace ws) : rest) = ws <> whiteString rest
+whiteString (Right (Comment c) : rest) = "[" <> c <> "]" <> whiteString rest
+whiteString [] = ""
 
 grammar :: GrammarBuilder Grammar Grammar Parser String
 grammar Boolean{..} = Boolean{
@@ -73,15 +83,22 @@ grammar Boolean{..} = Boolean{
                                  <|> keyword "not" *> takeCharsWhile isSpace *> (Boolean.not <$> factor)
                                  <|> symbol "(" *> expr <* symbol ")")}
 
-storeWhiteSpace (ws, (ws', a)) = ([], (ws <> ws', a))
+storeWhiteSpace ([ws], (ws', a)) = ([], (ws' <> [ws], a))
+storeWhiteSpace (ws:wss, (ws', a)) = ([], (ws : ws' <> wss, a))
 
 instance {-# OVERLAPS #-} TokenParsing (Parser Grammar String) where
    someSpace = someLexicalSpace
    token p = p <* lexicalWhiteSpace
 
 instance {-# OVERLAPS #-} LexicalParsing (Parser Grammar String) where
-   lexicalWhiteSpace = do ws <- takeCharsWhile isSpace
-                          lift ([ws], ())
+   lexicalWhiteSpace = tmap (first (\ws-> [concat ws])) $
+                       do ws <- takeCharsWhile isSpace
+                          lift ([[Left $ WhiteSpace ws]], ())
+                          (lexicalComment *> lexicalWhiteSpace <|> pure ())
+   lexicalComment = do char '['
+                       comment <- takeCharsWhile (/= ']')
+                       char ']'
+                       lift ([[Right $ Comment comment]], ())
    identifierToken p = do ident <- p
                           guard (ident /= "True" && ident /= "False")
                           lexicalWhiteSpace
