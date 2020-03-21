6eb6590b992d6b66c5e16c2b82aeef0c8e623bf9
Mario <blamario@protonmail.com>
2020-03-21 09:25:02 -0400

    Replaced <> with raise2?(Left|Right)

--- grammatical-parsers/examples/BooleanTransformations.hs
+++ grammatical-parsers/examples/BooleanTransformations.hs
@@ -32,12 +32,13 @@ deriving instance (Show (f (AST f)), Show (f Bool), Show (f String)) => Show (AS
 instance Boolean.BooleanDomain (NodeWrap (AST NodeWrap)) where
    and = binary And
    or = binary Or
-   not = pure . Not
-   true = pure (Literal True)
-   false = pure (Literal False)
+   not = bare . Not
+   true = bare (Literal True)
+   false = bare (Literal False)
 
-binary :: Applicative f => (f (AST f) -> f (AST f) -> AST f) -> f (AST f) -> f (AST f) -> f (AST f)
-binary f a b = pure (f a b)
+binary :: (NodeWrap (AST NodeWrap) -> NodeWrap (AST NodeWrap) -> AST NodeWrap)
+       -> NodeWrap (AST NodeWrap) -> NodeWrap (AST NodeWrap) -> NodeWrap (AST NodeWrap)
+binary f a b = bare (f a b)
 
 type NodeWrap = (,) AttachedIgnorables
 data AttachedIgnorables = Trailing Ignorables
@@ -47,14 +48,6 @@ newtype Comment    = Comment String    deriving Show
 newtype WhiteSpace = WhiteSpace String deriving Show
 
 type Grammar = Boolean.Boolean (NodeWrap (AST NodeWrap))
-
-instance Semigroup AttachedIgnorables where
-   Trailing a <> Trailing b = Trailing (a <> b)
-   Parenthesized a b c <> Trailing d = Parenthesized a b (c <> d)
-   Trailing a <> Parenthesized b c d = Parenthesized (a <> b) c d
-   a <> b = error (show (a, b))
-instance Monoid AttachedIgnorables where
-   mempty = Trailing mempty
 
 main :: IO ()
 main = do args <- concat <$> getArgs
@@ -69,25 +62,36 @@ main = do args <- concat <$> getArgs
 reconstructed :: Int -> NodeWrap (AST NodeWrap) -> String
 reconstructed prec (ws, node) = serialized prec ws node
 
-simplified :: Monoid a => (a, AST ((,) a)) -> (a, AST ((,) a))
+simplified :: NodeWrap (AST NodeWrap) -> NodeWrap (AST NodeWrap)
 simplified e@(_, Literal{}) = e
 simplified e@(_, Variable{}) = e
 simplified (a, Not e) = case simplified e
-                        of (b, Literal True) -> (a <> b, Literal False)
-                           (b, Literal False) -> (a <> b, Literal True)
+                        of (b, Literal True) -> (swallow a b, Literal False)
+                           (b, Literal False) -> (swallow a b, Literal True)
                            e' -> (a, Not e')
 simplified (a, And l r) = case (simplified l, simplified r)
-                          of ((b, Literal False), _) -> (a <> b, Literal False)
-                             ((b, Literal True), (c, r')) -> (a <> b <> c, r')
-                             (_, (b, Literal False)) -> (a <> b, Literal False)
-                             ((b, l'), (c, Literal True)) -> (a <> b <> c, l')
+                          of ((b, Literal False), _) -> (raiseLeft a b, Literal False)
+                             ((b, Literal True), (c, r')) -> (raise2right a b c, r')
+                             (_, (b, Literal False)) -> (raiseRight a b, Literal False)
+                             ((b, l'), (c, Literal True)) -> (raise2left a b c, l')
                              (l', r') -> (a, And l' r')
 simplified (a, Or l r) =  case (simplified l, simplified r)
-                          of ((b, Literal False), (c, r')) -> (a <> b <> c, r')
-                             ((b, Literal True), _) -> (a <> b, Literal True)
-                             ((b, l'), (c, Literal False)) -> (a <> b <> c, l')
-                             (_, (b, Literal True)) -> (a <> b, Literal True)
+                          of ((b, Literal False), (c, r')) -> (raise2right a b c, r')
+                             ((b, Literal True), _) -> (raiseLeft a b, Literal True)
+                             ((b, l'), (c, Literal False)) -> (raise2left a b c, l')
+                             (_, (b, Literal True)) -> (raiseRight a b, Literal True)
                              (l', r') -> (a, Or l' r')
+
+swallow, raiseLeft, raiseRight :: AttachedIgnorables -> AttachedIgnorables -> AttachedIgnorables
+raise2left, raise2right :: AttachedIgnorables -> AttachedIgnorables -> AttachedIgnorables -> AttachedIgnorables
+swallow = raiseRight
+raiseLeft   (Trailing a) (Trailing b) = Trailing (b <> a)
+raiseRight  (Trailing a) (Parenthesized b c d) = Parenthesized (a <> b) c d
+raiseRight  (Trailing a) (Trailing b) = Trailing (a <> b)
+raise2left  (Trailing a) (Trailing b) (Trailing c) = Trailing (a <> b <> c)
+raise2left  (Parenthesized a b c) d _ = Parenthesized a (raiseLeft b d) c
+raise2right (Trailing a) (Trailing b) (Trailing c) = Trailing (a <> b <> c)
+raise2right (Trailing a) (Trailing b) (Parenthesized c d e) = Parenthesized (a <> c) d e
 
 serialized :: Int -> AttachedIgnorables -> AST NodeWrap -> String
 serialized prec (Trailing follow) (Or left right)
@@ -114,9 +118,12 @@ grammar Boolean{..} = Boolean{
          <|> trailingWhiteSpace (Boolean.and <$> factor <* symbol "&&" <*> term),
    factor= trailingWhiteSpace (keyword "True" *> pure Boolean.true
                                  <|> keyword "False" *> pure Boolean.false
-                                 <|> pure . Variable <$> identifier
+                                 <|> bare . Variable <$> identifier
                                  <|> keyword "not" *> (Boolean.not <$> factor))
            <|> parenthesizedWhiteSpace (symbol "(" *> expr <* symbol ")")}
+
+bare :: a -> NodeWrap a
+bare a = (Trailing [], a)
 
 trailingWhiteSpace, parenthesizedWhiteSpace :: Parser Grammar String (NodeWrap (AST NodeWrap))
                                             -> Parser Grammar String (NodeWrap (AST NodeWrap))
