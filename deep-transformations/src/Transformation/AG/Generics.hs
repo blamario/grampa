b27323b695d810db44dc6b14b4e7b318c202cd8d
Mario <blamario@protonmail.com>
2020-09-13 09:35:40 -0400

    Added the Replicated newtype

--- deep-transformations/src/Transformation/AG/Generics.hs
+++ deep-transformations/src/Transformation/AG/Generics.hs
@@ -1,15 +1,18 @@
 {-# Language DataKinds, DefaultSignatures, FlexibleContexts, FlexibleInstances,
              MultiParamTypeClasses, PolyKinds, RankNTypes, ScopedTypeVariables, StandaloneDeriving,
-             TypeFamilies, TypeOperators, UndecidableInstances #-}
+             TypeApplications, TypeFamilies, TypeOperators, UndecidableInstances #-}
 
 module Transformation.AG.Generics where
 
+import Data.Functor.Compose (Compose(..))
 import Data.Functor.Const (Const(..))
+import Data.Functor.Identity (Identity(..))
 import Data.Kind (Type)
 import Data.Generics.Product.Subtype (Subtype(upcast))
 import Data.Proxy (Proxy(..))
 import GHC.Generics
 import qualified GHC.Generics as Generics
+import GHC.Records
 import GHC.TypeLits (Symbol, KnownSymbol, SomeSymbol(..), ErrorMessage (Text), TypeError)
 import qualified Rank2
 import Transformation (Transformation, Domain, Codomain)
@@ -53,7 +56,9 @@ instance (Atts (Synthesized t) (g sem sem) ~ result, Generic result, sem ~ Seman
 
 newtype PassDown (t :: Type) (f :: * -> *) a = PassDown a
 newtype Accumulated a = Accumulated a
+newtype Replicated m n a = Replicated (m a)
 data Accumulator (t :: Type) (name :: Symbol) (a :: Type) = Accumulator
+data Replicator (t :: Type) (m :: Type -> Type) (n :: Type -> Type) (name :: Symbol) (a :: Type) = Replicator
 
 instance Transformation (PassDown t f a) where
   type Domain (PassDown t f a) = f
@@ -63,12 +68,20 @@ instance Transformation (Accumulator t name a) where
   type Domain (Accumulator t name a) = Synthesized t
   type Codomain (Accumulator t name a) = Const a
 
+instance Transformation (Replicator t m n name a) where
+  type Domain (Replicator t m n name a) = Synthesized t
+  type Codomain (Replicator t m n name a) = Compose m n
+
 instance Subtype (Atts (Inherited t) a) b => Transformation.At (PassDown t f b) a where
    ($) (PassDown i) _ = Inherited (upcast i)
 
 instance (Monoid a, r ~ Atts (Synthesized t) x, Generic r, MayHaveMonoidalField name a (Rep r)) =>
          Transformation.At (Accumulator t name a) x where
    _ $ Synthesized r = Const (getMonoidalField (Proxy :: Proxy name) $ from r)
+
+instance (Applicative m, Applicative n, HasField name (Atts (Synthesized t) a) a) =>
+         Transformation.At (Replicator t m n name a) a where
+   _ $ Synthesized r = pure (getField @name r)
 
 class MayHaveMonoidalField (name :: Symbol) a f where
    getMonoidalField :: Proxy name -> f x -> a
@@ -101,6 +114,11 @@ instance SynthesizerField t g deep shallow name a => SynthesizerField' t g deep 
 instance  {-# overlappable #-} (Monoid a, Shallow.Foldable (Accumulator t name a) (g (Semantics t))) =>
                                SynthesizerField t g deep shallow name (Accumulated a) where
    synthesisF t _ _ s name = Accumulated (Shallow.foldMap (Accumulator :: Accumulator t name a) s)
+
+instance  {-# overlappable #-} (Applicative m, a ~ g (Semantics t) n,
+                                Shallow.Traversable (Replicator t m n name a) (g (Semantics t))) =>
+                               SynthesizerField t g deep shallow name (Replicated m n a) where
+   synthesisF t _ _ s name = Replicated (Shallow.traverse (Replicator :: Replicator t m n name a) s)
 
 bequestDefault, passDown :: forall sem shallow t g.
                             (sem ~ Semantics t, Domain t ~ shallow, Revelation t,
