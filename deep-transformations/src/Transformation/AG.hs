6d82807dc38c5e4d348cc88cf093da1af6a91f96
Mario Blažević <blamario@protonmail.com>
2021-04-17 22:24:26 -0400

    Added knitKeeping and applyDefaultWithAttributes to AG

--- deep-transformations/src/Transformation/AG.hs
+++ deep-transformations/src/Transformation/AG.hs
@@ -1,5 +1,5 @@
 {-# Language FlexibleContexts, FlexibleInstances,
-             MultiParamTypeClasses, RankNTypes, StandaloneDeriving,
+             MultiParamTypeClasses, RankNTypes, ScopedTypeVariables, StandaloneDeriving,
              TypeFamilies, TypeOperators, UndecidableInstances #-}
 
 -- | An attribute grammar is a particular kind of 'Transformation' that assigns attributes to nodes in a
@@ -8,7 +8,10 @@
 
 module Transformation.AG where
 
+import Unsafe.Coerce (unsafeCoerce)
+
 import qualified Rank2
+import Transformation (Domain)
 
 -- | Type family that maps a node type to the type of its attributes, indexed per type constructor.
 type family Atts (f :: * -> *) a
@@ -25,11 +28,19 @@ deriving instance (Show (Atts (Synthesized t) a)) => Show (Synthesized t a)
 -- attributes.
 type Semantics t = Inherited t Rank2.~> Synthesized t
 
+-- | A node's 'PreservingSemantics' is a natural tranformation from the node's inherited attributes to all its
+-- attributes paired with the preserved node.
+type PreservingSemantics t f = Rank2.Arrow (Inherited t) (Rank2.Product (AllAtts t) f)
+
+-- | All inherited and synthesized attributes
+data AllAtts t a = AllAtts{allInh :: Atts (Inherited t) a,
+                           allSyn :: Atts (Synthesized t) a}
+
 -- | An attribution rule maps a node's inherited attributes and its child nodes' synthesized attributes to the node's
 -- synthesized attributes and the children nodes' inherited attributes.
 type Rule t g =  forall sem . sem ~ Semantics t
-              => (Inherited   t (g sem sem), g sem (Synthesized t))
-              -> (Synthesized t (g sem sem), g sem (Inherited t))
+              => (Inherited   t (g sem (Semantics t)), g sem (Synthesized t))
+              -> (Synthesized t (g sem (Semantics t)), g sem (Inherited t))
 
 -- | The core function to tie the recursive knot, turning a 'Rule' for a node into its 'Semantics'.
 knit :: (Rank2.Apply (g sem), sem ~ Semantics t) => Rule t g -> g sem sem -> sem (g sem sem)
@@ -37,6 +48,28 @@ knit r chSem = Rank2.Arrow knit'
    where knit' inherited = synthesized
             where (synthesized, chInh) = r (inherited, chSyn)
                   chSyn = chSem Rank2.<*> chInh
+
+-- | Another way to tie the recursive knot, using a 'Rule' to add 'AllAtts' information to every node
+knitKeeping :: forall t f g sem. (sem ~ PreservingSemantics t f, Rank2.Apply (g sem),
+                              Atts (Inherited t) (g sem sem) ~ Atts (Inherited t) (g (Semantics t) (Semantics t)),
+                              Atts (Synthesized t) (g sem sem) ~ Atts (Synthesized t) (g (Semantics t) (Semantics t)),
+                              g sem (Synthesized t) ~ g (Semantics t) (Synthesized t),
+                              g sem (Inherited t) ~ g (Semantics t) (Inherited t))
+            => (forall a. f a -> a) -> Rule t g -> f (g (PreservingSemantics t f) (PreservingSemantics t f))
+            -> PreservingSemantics t f (g (PreservingSemantics t f) (PreservingSemantics t f))
+knitKeeping extract rule x = Rank2.Arrow knitted
+   where knitted :: Inherited t (g (PreservingSemantics t f) (PreservingSemantics t f))
+                 -> Rank2.Product (AllAtts t) f (g (PreservingSemantics t f) (PreservingSemantics t f))
+         chSem :: g (PreservingSemantics t f) (PreservingSemantics t f)
+         knitted inherited = Rank2.Pair AllAtts{allInh= inh inherited, allSyn= syn synthesized} x
+            where chInh :: g (PreservingSemantics t f) (Inherited t)
+                  chSyn :: g (PreservingSemantics t f) (Synthesized t)
+                  chKept :: g (PreservingSemantics t f) (Rank2.Product (AllAtts t) f)
+                  synthesized :: Synthesized t (g (PreservingSemantics t f) (PreservingSemantics t f))
+                  (synthesized, chInh) = unsafeCoerce (rule (unsafeCoerce inherited, unsafeCoerce chSyn))
+                  chSyn = Synthesized . allSyn . Rank2.fst Rank2.<$> chKept
+                  chKept = chSem Rank2.<*> chInh
+         chSem = extract x
 
 -- | The core type class for defining the attribute grammar. The instances of this class typically have a form like
 --
@@ -60,3 +93,15 @@ applyDefault :: (q ~ Semantics t, x ~ g q q, Rank2.Apply (g q), Attribution t g 
              => (forall a. p a -> a) -> t -> p x -> q x
 applyDefault extract t x = knit (attribution t x) (extract x)
 {-# INLINE applyDefault #-}
+
+-- | Drop-in implementation of 'Transformation.$' that preserves all attributes with every original node
+applyDefaultWithAttributes :: (p ~ Domain t, q ~ PreservingSemantics t p, x ~ g q q, Rank2.Apply (g q),
+                               Atts (Inherited t) (g q q) ~ Atts (Inherited t) (g (Semantics t) (Semantics t)),
+                               Atts (Synthesized t) (g q q) ~ Atts (Synthesized t) (g (Semantics t) (Semantics t)),
+                               g q (Synthesized t) ~ g (Semantics t) (Synthesized t),
+                               g q (Inherited t) ~ g (Semantics t) (Inherited t),
+                               Attribution t g (PreservingSemantics t p) p)
+                           => (forall a. p a -> a) -> t -> p (g (PreservingSemantics t p) (PreservingSemantics t p))
+                           -> PreservingSemantics t p (g (PreservingSemantics t p) (PreservingSemantics t p))
+applyDefaultWithAttributes extract t x = knitKeeping extract (attribution t x) x
+{-# INLINE applyDefaultWithAttributes #-}
