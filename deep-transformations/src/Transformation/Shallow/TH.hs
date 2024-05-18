a86a99ef6e692ba51845dfb8fcc5b31d0cd9e1d0
Mario Blažević <blamario@protonmail.com>
2024-05-18 19:27:33 -0400

    Generalized the TH generation code

--- deep-transformations/src/Transformation/Shallow/TH.hs
+++ deep-transformations/src/Transformation/Shallow/TH.hs
@@ -105,12 +105,12 @@ reifyConstructors ty = do
    tyVars' <- traverse reifyTVKindSynonyms tyVars
 
 #if MIN_VERSION_template_haskell(2,17,0)
-   let (KindedTV tyVar _ (AppT (AppT ArrowT StarT) StarT) : _) = reverse tyVars'
+   let (KindedTV tyVar _ (AppT (AppT ArrowT _) StarT) : _) = reverse tyVars'
        instanceType           = foldl apply (conT tyConName) (reverse $ drop 1 $ reverse tyVars')
        apply t (PlainTV name _)    = appT t (varT name)
        apply t (KindedTV name _ _) = appT t (varT name)
 #else
-   let (KindedTV tyVar  (AppT (AppT ArrowT StarT) StarT) : _) = reverse tyVars'
+   let (KindedTV tyVar  (AppT (AppT ArrowT _) StarT) : _) = reverse tyVars'
        instanceType           = foldl apply (conT tyConName) (reverse $ drop 1 $ reverse tyVars')
        apply t (PlainTV name)    = appT t (varT name)
        apply t (KindedTV name _) = appT t (varT name)
