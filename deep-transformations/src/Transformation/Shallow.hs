6cdeb485a4ae57f3fcaa48b14aef6d72d23d5c0e
Mario Blažević <blamario@protonmail.com>
2024-12-20 22:39:30 -0500

    Added Shallow class instances for Rank2.Empty, Proxy, Const, Only, and Identity

--- deep-transformations/src/Transformation/Shallow.hs
+++ deep-transformations/src/Transformation/Shallow.hs
@@ -7,11 +7,14 @@
 
 module Transformation.Shallow where
 
-import Control.Applicative (Applicative, liftA2)
-import Data.Functor.Compose (Compose)
-import Data.Functor.Const (Const)
+import Control.Applicative (Applicative, liftA2, pure)
+import qualified Data.Functor as Rank1
+import Data.Functor.Compose (Compose(getCompose))
+import Data.Functor.Const (Const(Const, getConst))
+import Data.Proxy (Proxy(Proxy))
 import qualified Rank2
-import           Transformation (Transformation, Domain, Codomain)
+import           Transformation (Transformation, Domain, Codomain, At)
+import qualified Transformation
 
 import Prelude hiding (Foldable(..), Traversable(..), Functor(..), Applicative(..), (<$>), fst, snd)
 
@@ -27,6 +30,51 @@ class (Transformation t, Rank2.Foldable g) => Foldable t g where
 -- | Like Rank2.'Rank2.Traversable' except it takes a 'Transformation' instead of a polymorphic function
 class (Transformation t, Rank2.Traversable g) => Traversable t g where
    traverse :: Codomain t ~ Compose m f => t -> g (Domain t) -> m (g f)
+
+instance Transformation t => Functor t Rank2.Empty where
+   _ <$> Rank2.Empty = Rank2.Empty
+
+instance Transformation t => Functor t Proxy where
+   _ <$> _ = Proxy
+
+instance Transformation t => Functor t (Const a) where
+   _ <$> Const a = Const a
+
+instance (Transformation t, t `At` a) => Functor t (Rank2.Only a) where
+   t <$> Rank2.Only x = Rank2.Only (t Transformation.$ x)
+
+instance Functor t g => Functor t (Rank2.Identity g) where
+   f <$> Rank2.Identity g = Rank2.Identity (f <$> g)
+
+instance Transformation t => Foldable t Rank2.Empty where
+   foldMap _ _ = mempty
+
+instance Transformation t => Foldable t Proxy where
+   foldMap _ _ = mempty
+
+instance Transformation t => Foldable t (Const x) where
+   foldMap _ _ = mempty
+
+instance (Transformation t, t `At` a, Codomain t ~ Const m) => Foldable t (Rank2.Only a) where
+   foldMap t (Rank2.Only x) = getConst (t Transformation.$ x)
+
+instance Foldable t g => Foldable t (Rank2.Identity g) where
+   foldMap f (Rank2.Identity g) = foldMap f g
+
+instance (Transformation t, Codomain t ~ Compose m f, Applicative m) => Traversable t Rank2.Empty where
+   traverse _ _ = pure Rank2.Empty
+
+instance (Transformation t, Codomain t ~ Compose m f, Applicative m) => Traversable t Proxy where
+   traverse _ _ = pure Proxy
+
+instance (Transformation t, Codomain t ~ Compose m f, Applicative m) => Traversable t (Const x) where
+   traverse _ (Const x) = pure (Const x)
+
+instance (Transformation t, t `At` a, Codomain t ~ Compose m f, Rank1.Functor m) => Traversable t (Rank2.Only a) where
+   traverse t (Rank2.Only x) = Rank2.Only Rank1.<$> getCompose (t Transformation.$ x)
+
+instance (Traversable t g, Codomain t ~ Compose m f, Rank1.Functor m) => Traversable t (Rank2.Identity g) where
+   traverse f (Rank2.Identity g) = Rank2.Identity Rank1.<$> traverse f g
 
 instance (Functor t g, Functor t h) => Functor t (Rank2.Product g h) where
    t <$> Rank2.Pair left right = Rank2.Pair (t <$> left) (t <$> right)
