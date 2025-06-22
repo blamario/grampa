b55126e57136e173fba8113266ad5b89f6a76b8b
Mario Blažević <blamario@protonmail.com>
2025-06-22 12:41:59 +0200

    Turned doctests from a testsuite into a named library, dropped cabal-doctest

--- deep-transformations/test/Doctest.hs
+++
@@ -1,8 +0,0 @@
-import Build_doctests (flags, pkgs, module_sources)
-import Test.DocTest (doctest)
-
-main :: IO ()
-main = do
-    doctest (flags ++ pkgs ++ module_sources)
-    doctest (flags ++ pkgs ++ ["-pgmL", "markdown-unlit", "-isrc", "test/README.lhs"])
-    doctest (flags ++ pkgs ++ ["-isrc", "test/RepMin.hs", "test/RepMinAuto.hs"])
--- rank2classes/test/Doctest.hs
+++
@@ -1,5 +0,0 @@
-import Build_doctests (flags, pkgs, module_sources)
-import Test.DocTest (doctest)
-
-main = do doctest (flags ++ pkgs ++ module_sources)
-          doctest (flags ++ pkgs ++ ["-pgmL", "markdown-unlit", "test/MyModule.lhs"])
