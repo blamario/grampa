16ee1588841c0e539481fd12834116535aff2ebb
Mario Blažević <blamario@protonmail.com>
2026-01-16 21:06:43 -0500

    Fixed bug #32

--- deep-transformations/src/Transformation/Deep/TH.hs
+++ deep-transformations/src/Transformation/Deep/TH.hs
@@ -109,12 +109,12 @@ reifyConstructors ty = do
    tyVars' <- traverse reifyTVKindSynonyms tyVars
 #if MIN_VERSION_template_haskell(2,17,0)
    let (KindedTV tyVar _ (AppT (AppT ArrowT StarT) StarT) :
-        KindedTV tyVar' _ (AppT (AppT ArrowT StarT) StarT) : _) = reverse tyVars'
+        KindedTV tyVar' _ _ : _) = reverse tyVars'
        apply t (PlainTV name _)    = appT t (varT name)
        apply t (KindedTV name _ _) = appT t (varT name)
 #else
    let (KindedTV tyVar  (AppT (AppT ArrowT StarT) StarT) :
-        KindedTV tyVar' (AppT (AppT ArrowT StarT) StarT) : _) = reverse tyVars'
+        KindedTV tyVar' _ : _) = reverse tyVars'
        apply t (PlainTV name)    = appT t (varT name)
        apply t (KindedTV name _) = appT t (varT name)
 #endif
