6af7d4ab8a0aa7b6767bbef9a2a6cb8ceaa093c8
Mario Blažević <blamario@protonmail.com>
2026-01-23 15:53:52 -0500

    Added (foldMap/traverse)(Inward/Outward)

--- deep-transformations/src/Transformation/Rank2.hs
+++ deep-transformations/src/Transformation/Rank2.hs
@@ -11,6 +11,7 @@ import Data.Kind (Type)
 import           Transformation (Transformation, Domain, Codomain)
 import qualified Transformation
 import qualified Transformation.Deep as Deep
+import qualified Transformation.Full as Full
 
 -- | Transform (naturally) the containing functor of every node in the given tree.
 (<$>) :: Deep.Functor (Map p q) g => (forall a. p a -> q a) -> g p p -> g q q
@@ -21,9 +22,26 @@ infixl 4 <$>
 foldMap :: (Deep.Foldable (Fold p m) g, Monoid m) => (forall a. p a -> m) -> g p p -> m
 foldMap f = Deep.foldMap (Fold f)
 
+-- | Fold the containing functor of every node in the given tree inward from the leaves to the root.
+foldMapInward :: (Deep.Foldable (Full.Inward (Fold p m)) g, Monoid m) => (forall a. p a -> m) -> g p p -> m
+foldMapInward f = Deep.foldMap $ Full.Inward $ Fold f
+
+-- | Fold the containing functor of every node in the given tree outward from the root to the leaves.
+foldMapOutward :: (Deep.Foldable (Full.Outward (Fold p m)) g, Monoid m) => (forall a. p a -> m) -> g p p -> m
+foldMapOutward f = Deep.foldMap $ Full.Outward $ Fold f
+
 -- | Traverse the containing functors of all nodes in the given tree.
 traverse :: Deep.Traversable (Traversal p q m) g => (forall a. p a -> m (q a)) -> g p p -> m (g q q)
 traverse f = Deep.traverse (Traversal f)
+
+-- | Traverse the containing functors of all nodes in the given tree inward from the leaves to the root.
+traverseInward :: Deep.Traversable (Full.Inward (Traversal p q m)) g => (forall a. p a -> m (q a)) -> g p p -> m (g q q)
+traverseInward f = Deep.traverse $ Full.Inward $ Traversal f
+
+-- | Traverse the containing functors of all nodes in the given tree outward from the root to the leaves.
+traverseOutward :: Deep.Traversable (Full.Outward (Traversal p q m)) g
+                => (forall a. p a -> m (q a)) -> g p p -> m (g q q)
+traverseOutward f = Deep.traverse $ Full.Outward $ Traversal f
 
 newtype Map (p :: Type -> Type) (q :: Type -> Type) = Map (forall x. p x -> q x)
 
